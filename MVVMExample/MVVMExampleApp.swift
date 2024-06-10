3e910fa947c7322f03f6c874c6369067f71becff
peppe.sapienza <sapienza.giuseppe93@gmail.com>
2024-06-10 13:39:27 +1000

    Initial Commit

---
+++ MVVMExample/MVVMExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMExampleApp.swift
+//  MVVMExample
+//
+//  Created by Giuseppe Sapienza on 10/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct MVVMExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
